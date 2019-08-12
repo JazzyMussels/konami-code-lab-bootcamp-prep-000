481219f102249f9e31347d28383f3ca2e7bfa1c9
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-12 17:08:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,34 +1,38 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
 
-function init() {
-  document.addEventListener('keydown', function(e) {
-   var index = 0
-   function onKeyDownHandler(e) {
-  const key = e.key
- 
-  if (key === codes[index]) {
-    index+=1;
- 
-    if (index === codes.length) {
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
       alert("Hurray!");
- 
-      index = 0;
+      konamiCodePosition = 0;
     }
   } else {
-    index = 0;
+    konamiCodePosition = 0;
   }
-   }
-  
-})
-}
+});
+
